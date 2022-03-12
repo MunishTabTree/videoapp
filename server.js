8336dafafc3fec6e5ce5b8355651004db9cec3b1
munish <munishkumar.m@tabtree.in>
2022-03-12 11:48:50 +0530

    changes

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const app = express();
-app.set('view engine', 'ejs')
-app.use(express.static('public'));
 const server = require("http").Server(app);
-app.get("/", (req, res) => {
-    // res.status(200).send("Hello World");
-    const roomId = 'ajhflkdsjhfk'
-    res.render('room', {roomId:roomId})
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
 });
-server.listen(3030);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
